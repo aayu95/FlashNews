d14114e4607bbfcc5b26492a0ef7a88c8a4cab7c
Abhishek Raina <araina97gmail.com>
2016-01-03 23:25:50 +0530

    Changed layout

--- app/src/main/java/com/example/abhishek/flashnews/MainActivity.java
+++ app/src/main/java/com/example/abhishek/flashnews/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public PersonViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.test_layout, parent, false);
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.test_item, parent, false);
             PersonViewHolder pvh = new PersonViewHolder(v);
             return pvh;
         }
